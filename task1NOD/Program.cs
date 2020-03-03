83b3644bf551eb4868d8c3df1bd765ede040648f
evgen <evgen.tula@gmail.com>
2020-03-03 17:35:05 +0300

    new projects 	new file:   Tasks.sln 	new file:   Tester/App.config 	new file:   Tester/ITask.cs 	new file:   Tester/Program.cs 	new file:   Tester/Task/simple/LuckyTickets/LuckyTickets.cs 	new file:   Tester/Task/simple/StringLength/StringLength.cs 	new file:   Tester/Tester.cs 	new file:   Tester/tester.csproj 	new file:   task1Fibonacci/Program.cs 	new file:   task1Fibonacci/task1Fibonacci.csproj 	new file:   task1NOD/Program.cs 	new file:   task1NOD/task1NOD.csproj 	new file:   task1POW/Program.cs 	new file:   task1POW/task1POW.csproj 	new file:   tast1CountSimpleNum/Program.cs 	new file:   tast1CountSimpleNum/tast1CountSimpleNum.csproj 	deleted:    Tester/.vs/Tester/v16/Server/sqlite3/db.lock 	deleted:    Tester/.vs/Tester/v16/Server/sqlite3/storage.ide 	deleted:    Tester/Tester.sln 	deleted:    Tester/Tester/App.config 	deleted:    Tester/Tester/ITask.cs 	deleted:    Tester/Tester/Program.cs 	deleted:    Tester/Tester/Task/simple/LuckyTickets/LuckyTickets.cs 	deleted:    Tester/Tester/Task/simple/StringLength/StringLength.cs 	deleted:    Tester/Tester/Tester.cs 	deleted:    Tester/Tester/Tester.csproj 	deleted:    Tester/Tester/bin/Debug/Tester.exe.config 	deleted:    Tester/Tester/obj/Debug/Tester.csproj.FileListAbsolute.txt

---
+++ task1NOD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1NOD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tast1CountSimpleNum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tast1CountSimpleNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
