31062051b0c9c8e37c77bbc7d402f822c00a6940
evgen <evgen.tula@gmail.com>
2020-03-24 18:15:16 +0300

    upd FEN

--- task2FEN/FEN.cs
+++ task2FEN/FEN.cs
@@ -5,9 +5,9 @@ using System.Text;
 
 namespace task2FEN
 {
-    enum Piece
+    enum Piece : int
     {
-        whitePawns,
+        whitePawns = 0,
         whiteKnights,
         whiteBishops,
         whiteRooks,
@@ -27,52 +27,75 @@ namespace task2FEN
         public override string Run(string[] data)
         {
             ulong[] board = new ulong[12];
-            //2 ^ (64 - 1)
-            ulong f = Math.Pow(2,64);
-            string position = "7k/8/8/8/8/8/8/K7";//data[0];
-            foreach(var item in position)
+            //2 ^ (64 - 1) == 9223372036854775808
+            ulong position = 9223372036854775808;//Math.Pow(2,64);
+            string positions = "7k/8/8/8/8/8/8/K7";//data[0];
+            position >>= 7;
+            foreach (char item in positions)
             {
+                if (item == '/')
+                {
+                    position >>= 8;
+                    continue;
+                }
+
+
                 switch (item)
                 {
+                    //K, Q, R, B, N, P — белые король, ферзь, ладья, слон, конь, пешка. 
+                    //k, q, r, b, n, p — чёрные король, ферзь, ладья, слон, конь, пешка.
                     case 'K':
+                        board[(int)Piece.whiteKing] += position;
                         break;
 
                     case 'Q':
+                        board[(int)Piece.whiteQueens] += position;
                         break;
 
                     case 'R':
+                        board[(int)Piece.whiteRooks] += position;
                         break;
 
                     case 'B':
+                        board[(int)Piece.whiteBishops] += position;
                         break;
 
                     case 'N':
+                        board[(int)Piece.whiteKnights] += position;
                         break
                             ;
                     case 'P':
+                        board[(int)Piece.whitePawns] += position;
                         break;
 
 
                     case 'k':
+                        board[(int)Piece.blackKing] += position;
                         break;
 
                     case 'q':
+                        board[(int)Piece.blackQueens] += position;
                         break;
 
                     case 'r':
+                        board[(int)Piece.blackRooks] += position;
                         break;
 
                     case 'b':
+                        board[(int)Piece.blackBishops] += position;
                         break;
 
                     case 'n':
+                        board[(int)Piece.blackKnights] += position;
                         break;
 
                     case 'p':
+                        board[(int)Piece.blackPawns] += position;
                         break;
 
                     default:
-                        f = f << Convert.ToInt32(item);
+
+                        position = position << (int)Char.GetNumericValue(item);
                         break;
 
                 }
---
+++ task2FEN/Test/Result.md
@@ -0,0 +1,3 @@
+<table border="1">
+<caption>FEN - BITS (Fen)</caption>
+<tr><th>Test</th><th>Time (ms)</th></tr>
