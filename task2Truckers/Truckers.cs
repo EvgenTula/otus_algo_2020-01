3039c52f9ce4ef48077675021cc122b38424219e
evgen <evgen.tula@gmail.com>
2020-03-26 12:02:24 +0300

    tmp

--- task2Truckers/Truckers.cs
+++ task2Truckers/Truckers.cs
@@ -24,91 +24,8 @@ namespace task2Truckers
     class Truckers : Task
     {
         public override string Run(string[] data)
-        {
-            /*
-            int startPosition = 18;
-            ulong whiteRooks = 1ul << startPosition;
-            ulong nL = 0xFEFEFEFEFEFEFEFE;
-            ulong nR = 0x7F7F7F7F7F7F7F7F;           
-            ulong maskWhiteRooks = 0;
-            
-            //up
-            ulong stepRooks = whiteRooks << 8;
-            while (stepRooks != 0)
-            {
-
-                maskWhiteRooks |= stepRooks;
-                stepRooks <<= 8;
-            }
-
-            //down
-            stepRooks = whiteRooks >> 8;
-            while(stepRooks != 0)
-            {
-                maskWhiteRooks |= stepRooks;
-                stepRooks >>= 8;
-            }
-
-
-            //left
-            stepRooks = whiteRooks >> 1;
-            while (stepRooks != 0)
-            {
-                maskWhiteRooks |= stepRooks;
-                stepRooks = (stepRooks & nL) >> 1;
-            }
-            //right
-            stepRooks = whiteRooks << 1;
-            while (stepRooks != 0)
-            {
-                maskWhiteRooks |= stepRooks;
-                stepRooks = (stepRooks & nR) << 1;
-            } 
-            */
-
-
-
-            int startPosition = 18;
-            ulong whiteRooks = 1ul << startPosition;
-            ulong nL = 0xFEFEFEFEFEFEFEFE;
-            ulong nR = 0x7F7F7F7F7F7F7F7F;
-            ulong maskWhiteRooks = 0;
-            
-            //up-left
-            ulong stepRooks = whiteRooks << 8;
-            while (stepRooks != 0)
-            {
-
-                maskWhiteRooks |= stepRooks;
-                stepRooks <<= 8;
-            }
-            /*
-            //down
-            stepRooks = whiteRooks >> 8;
-            while (stepRooks != 0)
-            {
-                maskWhiteRooks |= stepRooks;
-                stepRooks >>= 8;
-            }
-
-
-            //left
-            stepRooks = whiteRooks >> 1;
-            while (stepRooks != 0)
-            {
-                maskWhiteRooks |= stepRooks;
-                stepRooks = (stepRooks & nL) >> 1;
-            }
-            //right
-            stepRooks = whiteRooks << 1;
-            while (stepRooks != 0)
-            {
-                maskWhiteRooks |= stepRooks;
-                stepRooks = (stepRooks & nR) << 1;
-            }*/
-            int a = 0;
-            /*
-            int[] board = new int[64];
+        {        
+            ulong[] board = new ulong[64];
             int index = 63;
             //2 ^ (64 - 1) == 9223372036854775808
             ulong position = 9223372036854775808;
@@ -175,7 +92,7 @@ namespace task2Truckers
                 }
             }
             
-             */
+             
             /*
             String result = "";
 
@@ -189,13 +106,187 @@ namespace task2Truckers
             return "";
 
 
-            /*
+            
             void fillBoard(Piece piece)
             {
                 board[index] = (int)piece;
                 currentPosition <<= 1;
             };
  */           
+        }
+        
+        private ulong WhiteRooksMask(int startPosition, ulong[] board)
+        {
+            ulong whiteRooks = 1ul << startPosition;
+            ulong nL = 0xFEFEFEFEFEFEFEFE;
+            ulong nR = 0x7F7F7F7F7F7F7F7F;
+            ulong maskWhiteRooks = 0;
+
+            //up
+            ulong stepRooks = whiteRooks << 8;
+            while (stepRooks != 0)
+            {
+
+                maskWhiteRooks |= stepRooks;
+                stepRooks <<= 8;
+            }
+
+            //down
+            stepRooks = whiteRooks >> 8;
+            while (stepRooks != 0)
+            {
+                maskWhiteRooks |= stepRooks;
+                stepRooks >>= 8;
+            }
+
+
+            //left
+            stepRooks = (whiteRooks & nL) >> 1;
+            while (stepRooks != 0)
+            {
+                maskWhiteRooks |= stepRooks;
+                stepRooks = (stepRooks & nL) >> 1;
+            }
+            //right
+            stepRooks = (whiteRooks & nR) << 1;
+            while (stepRooks != 0)
+            {
+                maskWhiteRooks |= stepRooks;
+                stepRooks = (stepRooks & nR) << 1;
+            }
+
+            return maskWhiteRooks;
+        }
+
+        private ulong WhiteBishopsMask(int startPosition, ulong[] board)
+        {
+            int startStep = 0;
+            ulong whiteBishops = 1ul << startPosition;
+            ulong nL = 0xFEFEFEFEFEFEFEFE;
+            ulong nR = 0x7F7F7F7F7F7F7F7F;
+            ulong maskWhiteBishops = 0;
+
+            //up-left            
+            startStep = 7;
+            ulong stepBishops = (whiteBishops & nL) << startStep;
+            while (stepBishops != 0)
+            {
+
+                maskWhiteBishops |= stepBishops;
+                stepBishops = (stepBishops & nL) << startStep;
+            }
+
+            //up-right
+            startStep = 9;
+            stepBishops = (whiteBishops & nR) << startStep;
+            while (stepBishops != 0)
+            {
+                maskWhiteBishops |= stepBishops;
+                stepBishops = (stepBishops & nR) << startStep;
+            }
+
+
+            //down-left
+            startStep = 9;
+            stepBishops = (whiteBishops & nL) >> startStep;
+            while (stepBishops != 0)
+            {
+                maskWhiteBishops |= stepBishops;
+                stepBishops = (stepBishops & nL) >> startStep;
+            }
+
+
+            //down-right
+            startStep = 7;
+            stepBishops = (whiteBishops & nR) >> startStep;
+            while (stepBishops != 0)
+            {
+                maskWhiteBishops |= stepBishops;
+                stepBishops = (stepBishops & nR) >> startStep;
+            }
+
+            return maskWhiteBishops;
+        }
+
+        private ulong WhileQueensMask(int startPosition, ulong[] board)
+        {
+            int startStep = 0;
+            ulong whiteQueens = 1ul << startPosition;
+            ulong nL = 0xFEFEFEFEFEFEFEFE;
+            ulong nR = 0x7F7F7F7F7F7F7F7F;
+            ulong maskWhiteQueens = 0;
+            ulong stepQueens = 0;
+
+            //left
+            stepQueens = (whiteQueens & nL) >> 1;
+            while (stepQueens != 0)
+            {
+                maskWhiteQueens |= stepQueens;
+                stepQueens = (stepQueens & nL) >> 1;
+            }
+
+            //up-left            
+            startStep = 7;
+            stepQueens = (whiteQueens & nL) << startStep;
+            while (stepQueens != 0)
+            {
+
+                maskWhiteQueens |= stepQueens;
+                stepQueens = (stepQueens & nL) << startStep;
+            }
+
+            //up
+            stepQueens = whiteQueens << 8;
+            while (stepQueens != 0)
+            {
+
+                maskWhiteQueens |= stepQueens;
+                stepQueens <<= 8;
+            }
+
+            //up-right
+            startStep = 9;
+            stepQueens = (whiteQueens & nR) << startStep;
+            while (stepQueens != 0)
+            {
+                maskWhiteQueens |= stepQueens;
+                stepQueens = (stepQueens & nR) << startStep;
+            }
+
+            //right
+            stepQueens = (whiteQueens & nR) << 1;
+            while (stepQueens != 0)
+            {
+                maskWhiteQueens |= stepQueens;
+                stepQueens = (stepQueens & nR) << 1;
+            }
+
+            //down-right
+            startStep = 7;
+            stepQueens = (whiteQueens & nR) >> startStep;
+            while (stepQueens != 0)
+            {
+                maskWhiteQueens |= stepQueens;
+                stepQueens = (stepQueens & nR) >> startStep;
+            }
+
+            //down
+            stepQueens = whiteQueens >> 8;
+            while (stepQueens != 0)
+            {
+                maskWhiteQueens |= stepQueens;
+                stepQueens >>= 8;
+            }
+
+            //down-left
+            startStep = 9;
+            stepQueens = (whiteQueens & nL) >> startStep;
+            while (stepQueens != 0)
+            {
+                maskWhiteQueens |= stepQueens;
+                stepQueens = (stepQueens & nL) >> startStep;
+            }
+            return maskWhiteQueens;
         }
     }
 }
