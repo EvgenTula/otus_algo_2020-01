ccac68e15492094a09d2b205c3b5a4283fd4b531
evgen <supernet555@mail.ru>
2020-07-04 17:17:56 +0300

    rename

---
+++ Task16DynamicProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task16DynamicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
