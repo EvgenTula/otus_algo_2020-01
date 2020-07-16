3a66d351db55eb91abfa196a25f076cfbf2bf8c8
evgen <supernet555@mail.ru>
2020-07-16 20:56:06 +0300

    task 17 done

---
+++ Task17Probabilistic/BloomFilter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Security.Cryptography;
+
+namespace Task17Probabilistic
+{
+    public class BloomFilter
+    {
+        long size = 0; //M
+        int hashCount = 0; //k
+        BitArray BitArray;
+        HashAlgorithm hashAlgorithm;
+        public BloomFilter(double n, double p, HashAlgorithm algorithm)
+        {
+            size = (long)Math.Ceiling(-n * (Math.Log2(p) / Math.Log(2)));
+            hashCount = (int)Math.Ceiling(size / n * Math.Log(2));
+            BitArray = new BitArray((int)size);
+            hashAlgorithm = algorithm;
+        }
+
+        public void add(byte[] data)
+        {
+            var hash1 = GetHash(data);
+            var hash2 = GetHash(hash1);
+            for(int i = 0; i < hashCount; i++)
+            {                
+                int position = (int)Math.Abs((hash1 + i * hash2) % size);
+                BitArray.Set(position, true);
+            }
+        }
+
+        public bool constains(byte[] data)
+        {
+            var hash1 = GetHash(data);
+            var hash2 = GetHash(hash1);
+            for (int i = 0; i < hashCount; i++)
+            {
+                int position = (int)Math.Abs((hash1 + i * hash2) % size);
+                if (!BitArray.Get(position))
+                    return false;
+            }
+            return true;
+        }
+
+        public int getSize()
+        {
+            return BitArray.Length;
+        }
+
+        private int GetHash(byte[] data)
+        {
+            var hashValue = hashAlgorithm.ComputeHash(data);
+            return BitConverter.ToInt32(hashValue);
+        }
+
+        private int GetHash(int data)
+        {
+            return GetHash(BitConverter.GetBytes(data));
+        }
+    }
+}
---
+++ Task17Probabilistic/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Task17Probabilistic
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+
+            double countItem = 100_000;      //  N
+            double rate = 0.02;              //  p
+
+            Random random = new Random();
+            HashSet<byte[]> data = new HashSet<byte[]>();
+            int byteSize = 0;
+            while (data.Count < countItem)
+            {
+                byte[] chunk = new byte[random.Next(100)];
+                byteSize += chunk.Length;
+                random.NextBytes(chunk);                
+                data.Add(chunk);
+            }
+
+            int falsePositive = 0;
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                BloomFilter bloomFilter = new BloomFilter(countItem, rate, sha256Hash);
+                var index = data.GetEnumerator();
+                for(int i = 0; i < data.Count; i++)
+                {
+                    index.MoveNext();
+                    var chunk = index.Current;
+                    if (bloomFilter.constains(chunk))
+                        falsePositive++;
+                    bloomFilter.add(index.Current);
+                }
+
+                Console.WriteLine("bloomFilter size:\t" + bloomFilter.getSize());
+                Console.WriteLine("data size:\t\t" + byteSize);
+            }
+
+
+            Console.WriteLine("Percent error: " + falsePositive / countItem * 100 + "%");
+            Console.ReadKey();
+        }
+    }
+}
